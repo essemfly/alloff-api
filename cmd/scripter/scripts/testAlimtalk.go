df0dc6c052b386a82d369437649fee306f190c43
seokmin <6863829+essemfly@users.noreply.github.com>
2022-01-17 16:49:34 +0900

    Fix test alimtalk

---
+++ cmd/scripter/scripts/testAlimtalk.go
@@ -0,0 +1,43 @@
+package scripts
+
+import (
+	"log"
+	"time"
+
+	"github.com/lessbutter/alloff-api/config/ioc"
+	"github.com/lessbutter/alloff-api/internal/core/domain"
+	"github.com/lessbutter/alloff-api/internal/pkg/alimtalk"
+	"github.com/lessbutter/alloff-api/internal/utils"
+)
+
+func testAlimtalk() {
+	user, _ := ioc.Repo.Users.GetByMobile("01097711882")
+	newAlimtalk := &domain.AlimtalkDAO{
+		UserID:       user.ID.Hex(),
+		Mobile:       user.Mobile,
+		TemplateCode: domain.PAYMENT_OK,
+		ReferenceID:  "Test alimtalk payment ref id",
+		SendDate:     nil,
+		TemplateParams: map[string]string{
+			"orderedAt":   time.Now().String(),
+			"productName": "test payment id",
+			"amount":      utils.PriceFormatter(135000),
+		},
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	}
+
+	requestId, err := alimtalk.SendMessage(newAlimtalk)
+	if err != nil {
+		newAlimtalk.Status = domain.ALIMTALK_STATUS_FAILED
+	} else {
+		newAlimtalk.ToastRequestID = requestId
+		newAlimtalk.Status = domain.ALIMTALK_STATUS_COMPLETED
+	}
+
+	_, err = ioc.Repo.Alimtalks.Insert(newAlimtalk)
+	if err != nil {
+		log.Println(err)
+	}
+
+}
