c7dffbf0cf33504c894d1d6865042020e6d1b49e
seokmin <essemfly@gmail.com>
2022-01-24 20:11:48 +0900

    Make test code for notification sender

--- cmd/scripter/scripts/notificationAdder.go
+++ cmd/scripter/scripts/notificationAdder.go
@@ -1,17 +1,31 @@
 package scripts
 
-func AddProductDiffNotificaton() {
-	// diffs := internal.ListNotPushedProductPriceDiffs()
+import (
+	"log"
 
-	// for _, diff := range diffs {
-	// 	err := notification.InsertDiffNotification(diff.NewProduct, diff.OldProduct)
-	// 	if err != nil {
-	// 		log.Println("Error occured on diff ID", diff.ID, err)
-	// 	}
-	// 	err = internal.UpdatePriceProductDiffPushed(diff)
-	// 	if err != nil {
-	// 		log.Println("Error occured on updating diff ", diff.ID, err)
-	// 	}
-	// }
-	// log.Println("Insert Diff notifications finished")
+	"github.com/lessbutter/alloff-api/config/ioc"
+	"github.com/lessbutter/alloff-api/internal/pkg/notification"
+	"go.mongodb.org/mongo-driver/bson"
+)
+
+func AddProductDiffNotificaton() {
+	filter := bson.M{"ispushed": false, "type": "price"}
+	diffs, err := ioc.Repo.ProductDiffs.List(filter)
+	if err != nil {
+		log.Println("err", err)
+	}
+
+	for _, diff := range diffs {
+		err := notification.InsertDiffNotification(diff.NewProduct, diff.OldPrice)
+		if err != nil {
+			log.Println("Error occured on diff ID", diff.ID, err)
+		}
+
+		diff.IsPushed = true
+		_, err = ioc.Repo.ProductDiffs.Update(diff)
+		if err != nil {
+			log.Println("Error occured on updating diff ", diff.ID, err)
+		}
+	}
+	log.Println("Insert Diff notifications finished")
 }
