aeba726a440566b2d28d2cd27a306b81f11a7f0c
seokmin <essemfly@gmail.com>
2022-02-04 20:12:56 +0900

    Add temp timedeal messages

--- cmd/scripter/main.go
+++ cmd/scripter/main.go
@@ -31,9 +31,13 @@ func main() {
 	// scripts.AddProductDiffNotificaton()
 	// scripts.ConfirmOrders()
 	// scripts.SendNotification()
-	scripts.AddProductGroups()
+	// scripts.AddProductGroups()
 	// scripts.AddMockOrders()
-	scripts.AddMockHomeItems()
+	// scripts.AddMockHomeItems()
 	// scripts.InsertDiffNotification()
 	// scripts.TestAlimtalk()
+	title := "MaxMara(Intrend) 시즌오프:짠:"
+	message := "(광고) 품절임박! 시즌오프 세일:폭탄:"
+	timedealID := "61a9d41dee2c39d09bebeedb"
+	scripts.AddTimedealNotification(timedealID, title, message)
 }
---
+++ cmd/scripter/scripts/timedealNotificationAdder.go
@@ -0,0 +1,99 @@
+package scripts
+
+import (
+	"log"
+
+	"github.com/lessbutter/alloff-api/config/ioc"
+	"github.com/lessbutter/alloff-api/internal/core/domain"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+func AddTimedealNotification(timedealID, pushtitle, message string) error {
+	// Admins
+	adminDeviceIDs := []string{
+		"c0IfW-xzdUx3nu6XmpzXNz:APA91bGYmuaoWsdoNzhp_SqUiaKm_8bPK-MfUgmJpmvprUui6run4qKTEeF8QpPSnrR7f5oK2Suy5BJduO04C2DuKWmYHbYZJCRy_FtI6Rm6kAxEopwiRSGjymiqGzXVpz8i8nffFYV1", // 주영
+		"dJ4rSV2L60aLpHxQjxoFz-:APA91bGJR2YXQkmBbv0rELM6caPUeZ3C1MnBkz1wlI68wCzDRhc9Bsma3stSCRXTGip-6mxdtj2GfuMT4c0XV85AWDuLr0lkH33VDNRsuc8nqo24JGOHmDDpYl_wetLh9vYL-3I0A6ID", // 석민
+		"cBRNxXqZwEeapVVni8KJcG:APA91bG7UnRCfxWvNR7ngSYNfhTazApx9yAlQXtXqCJDWpn_X-cwVMnnUDLmjLUCso9s7_oiP_xrBkOqoa-1ie3LaRsckENluZTaxWcNAKpdUvVZtV9Pq_TRgRdmwtpA0kE_-Mx-_Nzl", // 명규
+		"eEx9IUeGQtGgo__aVsJih4:APA91bG0NaGdVP_2NyfBWAXXXdZ0-iR9J-pRTf6JxjueMowaIg9HQDYiZ5PkGeqTypblQNDg-Fecdask8W6o15lNHOhzCHfHzIzofE63APEZPNQQbRVjaaBCTk6m3kAeIqoBuJl9rmHh", // 석민 안드로이드
+	}
+
+	isTest := true
+
+	timedeal, err := ioc.Repo.ProductGroups.Get(timedealID)
+	if err != nil {
+		log.Println("err", err)
+		return err
+	}
+
+	if isTest {
+		timedealNoti := domain.NotificationDAO{
+			ID:               primitive.NewObjectID(),
+			Status:           domain.NOTIFICATION_READY,
+			NotificationType: domain.NOTIFICATION_TIMEDEAL_OPEN_NITIFICATION,
+			Title:            pushtitle,
+			Message:          message,
+			Notificationid:   "/timedeals/" + timedeal.ID.Hex(),
+			DeviceIDs:        adminDeviceIDs,
+			NavigateTo:       "/timedeals",
+			ReferenceID:      "/" + timedeal.ID.Hex(),
+		}
+
+		_, err := ioc.Repo.Notifications.Insert(&timedealNoti)
+		if err != nil {
+			log.Println(err)
+			return err
+		}
+		return nil
+	}
+
+	devices, err := ioc.Repo.Devices.ListAllowed()
+	if err != nil {
+		return err
+	}
+
+	deviceIDs := []string{}
+
+	for _, device := range devices {
+		deviceIDs = append(deviceIDs, device.DeviceId)
+		if len(deviceIDs) >= 300 {
+			timedealNoti := domain.NotificationDAO{
+				ID:               primitive.NewObjectID(),
+				Status:           domain.NOTIFICATION_READY,
+				NotificationType: domain.NOTIFICATION_TIMEDEAL_OPEN_NITIFICATION,
+				Title:            pushtitle,
+				Message:          message,
+				Notificationid:   "/timedeals/" + timedeal.ID.Hex(),
+				DeviceIDs:        deviceIDs,
+				NavigateTo:       "/timedeals",
+				ReferenceID:      "/" + timedeal.ID.Hex(),
+			}
+
+			_, err := ioc.Repo.Notifications.Insert(&timedealNoti)
+			if err != nil {
+				log.Println(err)
+				return err
+			}
+			deviceIDs = []string{}
+		}
+	}
+
+	timedealNoti := domain.NotificationDAO{
+		ID:               primitive.NewObjectID(),
+		Status:           domain.NOTIFICATION_READY,
+		NotificationType: domain.NOTIFICATION_TIMEDEAL_OPEN_NITIFICATION,
+		Title:            pushtitle,
+		Message:          message,
+		Notificationid:   "/timedeals/" + timedeal.ID.Hex(),
+		DeviceIDs:        deviceIDs,
+		NavigateTo:       "/timedeals",
+		ReferenceID:      "/" + timedeal.ID.Hex(),
+	}
+
+	_, err = ioc.Repo.Notifications.Insert(&timedealNoti)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	return nil
+}
