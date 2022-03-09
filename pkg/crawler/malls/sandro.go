81b98b08a81f71ffc54bf77e6f4e013f16a0f8c9
Stephen Cheong <j.stephen.cheong@gmail.com>
2022-03-09 17:30:50 +0900

    Implemented Sandro crawler

--- pkg/crawler/malls/sandro.go
+++ pkg/crawler/malls/sandro.go
@@ -3,177 +3,199 @@ package malls
 import (
 	"fmt"
 	"log"
-
-	// "strconv"
-	// "strings"
+	"regexp"
+	"strconv"
+	"strings"
 
 	"github.com/gocolly/colly"
-	// "github.com/lessbutter/alloff-api/config/ioc"
+	"github.com/lessbutter/alloff-api/config/ioc"
 	"github.com/lessbutter/alloff-api/internal/core/domain"
-	// "github.com/lessbutter/alloff-api/pkg/crawler"
-	// "github.com/lessbutter/alloff-api/pkg/product"
+	"github.com/lessbutter/alloff-api/pkg/crawler"
+	"github.com/lessbutter/alloff-api/pkg/product"
+)
+
+const (
+	allowedDomain           = "de.sandro-paris.com"
+	userAgent               = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11"
+	defaultStock            = 100
+	recommendingDescription = "passt perfekt zum"
+	referenceDescription    = "Référence"
+	inlineSizeDescription   = "Größenentsprechung"
+	modelDescription        = "Das Model"
 )
 
 func CrawlSandro(worker chan bool, done chan bool, source *domain.CrawlSourceDAO) {
-	c := colly.NewCollector(
-		colly.AllowedDomains("de.sandro-paris.com"),
-		colly.UserAgent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11"),
-	)
+	c := getCollyCollector()
+	totalProducts := 0
 
-	// brand, err := ioc.Repo.Brands.GetByKeyname(source.Category.BrandKeyname)
-	// if err != nil {
-	// log.Println(err)
-	// }
+	brand, err := ioc.Repo.Brands.GetByKeyname(source.Category.BrandKeyname)
+	if err != nil {
+		log.Println(err)
+	}
 
-	// currentPageNum := 0
-	// totalProducts := 0
-
-	c.OnHTML(".product-name", func(e *colly.HTMLElement) {
-		fmt.Println(e.ChildText(".name-link"))
-		// originalPriceStr := e.ChildText(".full-price")
-		// originalPrice := 0.0
-		// if originalPriceStr != "" {
-		// 	originalPriceStr = strings.Split(originalPriceStr, " ")[1]
-		// 	originalPriceStr = strings.Replace(originalPriceStr, ",", ".", -1)
-		// 	originalPrice, err = strconv.ParseFloat(originalPriceStr, 32)
-		// 	if err != nil {
-		// 		log.Println("err ", err)
-		// 		return
-		// 	}
-		// }
-
-		// discountedPriceStr := e.ChildText(".price")
-		// discountedPriceStr = strings.Split(discountedPriceStr, " ")[1]
-		// discountedPriceStr = strings.Replace(discountedPriceStr, ",", ".", -1)
-		// discountedPrice, err := strconv.ParseFloat(discountedPriceStr, 32)
-		// if err != nil {
-		// 	log.Println("err ", err)
-		// 	return
-		// }
-
-		// if discountedPrice == 0 {
-		// 	discountedPrice = originalPrice
-		// } else if originalPrice == 0.0 {
-		// 	originalPrice = discountedPrice
-		// }
-
-		// productID := e.Attr("data-product-id")
-		// productUrl := "https://it.intrend.it" + e.ChildAttr(".js-anchor", "href")
-
-		// title, images, sizes, colors, inventories, description := getIntrendDetail(productUrl)
-
-		// if len(sizes) == 0 {
-		// 	inventories = append(inventories, domain.InventoryDAO{
-		// 		Size:     "normal",
-		// 		Quantity: 10,
-		// 	})
-		// }
-		// addRequest := &product.ProductCrawlingAddRequest{
-		// 	Brand:         brand,
-		// 	Source:        source,
-		// 	ProductID:     productID,
-		// 	ProductName:   title,
-		// 	ProductUrl:    productUrl,
-		// 	Images:        images,
-		// 	Sizes:         sizes,
-		// 	Inventories:   inventories,
-		// 	Colors:        colors,
-		// 	Description:   description,
-		// 	OriginalPrice: float32(originalPrice),
-		// 	SalesPrice:    float32(discountedPrice),
-		// 	CurrencyType:  domain.CurrencyEUR,
-		// }
-
-		// totalProducts += 1
-		// product.AddProductInCrawling(addRequest)
+	c.OnHTML(".product-info", func(e *colly.HTMLElement) {
+		colorMap := map[string]string{}
+		e.ForEach("ul.swatch-list li", func(_ int, li *colly.HTMLElement) {
+			colorId := li.ChildAttr("img", "data-colorid")
+			colorName := li.ChildAttr("img", "data-colorname")
+			colorMap[colorId] = colorName
+		})
+		nameUrl := e.ChildAttr(".name-link", "href")
+		urlNodes := strings.Split(nameUrl, "/")
+		productId := strings.Split(urlNodes[len(urlNodes)-1], ".html")[0]
+		for colorId, colorName := range colorMap {
+			totalProducts++
+			productDetailUrl := getSandroDetailUrl(productId, colorId)
+			productName, images, sizes, inventories, description, originalPrice, salesPrice := getSandroDetail(productDetailUrl)
+			addRequest := &product.ProductCrawlingAddRequest{
+				Brand:         brand,
+				Images:        images,
+				Sizes:         sizes,
+				Inventories:   inventories,
+				Description:   description,
+				OriginalPrice: originalPrice,
+				SalesPrice:    salesPrice,
+				CurrencyType:  domain.CurrencyEUR,
+				Source:        source,
+				ProductID:     productId,
+				ProductName:   productName + " - " + colorName,
+				ProductUrl:    getSandroProductUrl(productId, colorId),
+			}
+			totalProducts += 1
+			product.AddProductInCrawling(addRequest)
+		}
 	})
 
-	// c.OnHTML(".js-pager .container-fluid ul", func(e *colly.HTMLElement) {
-	// 	lastPageStr := e.ChildAttr("li:last-child a", "data-page")
-	// 	lastPageNum, _ := strconv.Atoi(lastPageStr)
-	// 	if currentPageNum < lastPageNum {
-	// 		currentPageNum += 1
-	// 		url := source.CrawlUrl + "?page=" + strconv.Itoa(currentPageNum)
-	// 		c.Visit(url)
-	// 	}
-	// })
-	err := c.Visit(source.CrawlUrl)
+	err = c.Visit(source.CrawlUrl)
 	if err != nil {
 		log.Println("err occured in crawling sandro", err)
 	}
 
-	// crawler.PrintCrawlResults(source, totalProducts)
+	crawler.PrintCrawlResults(source, totalProducts)
 	<-worker
 	done <- true
 }
 
-// type IntrendStock struct {
-// 	STYCD    string `json:"STYCD"`
-// 	SIZECD   string `json:"SIZECD"`
-// 	COLCD    string `json:"COLCD"`
-// 	SIZECDNM string `json:"SIZECDNM"`
-// 	SALECNT  int    `json:"SALECNT"`
-// 	STOCKQTY int    `json:"STOCKQTY"`
-// }
+func getSandroDetailUrl(productId string, colorId string) string {
+	return getSandroProductUrl(productId, colorId) + "&format=ajax"
+}
 
-// func getIntrendDetail(productUrl string) (title string, imageUrls []string, sizes, colors []string, inventories []domain.InventoryDAO, description map[string]string) {
-// 	c := colly.NewCollector(
-// 		colly.AllowedDomains("it.intrend.it"),
-// 		colly.UserAgent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11"),
-// 	)
+func getSandroProductUrl(productId string, colorId string) string {
+	return fmt.Sprintf("https://de.sandro-paris.com/on/demandware.store/Sites-Sandro-DE-Site/de_DE/Product-Variation?pid=%s&dwvar_%s_color=%s&Quantity=1", productId, productId, colorId)
+}
 
-// 	description = map[string]string{}
+func getSandroDetail(productUrl string) (
+	productName string,
+	images []string,
+	sizes []string,
+	inventories []domain.InventoryDAO,
+	description map[string]string,
+	originalPrice float32,
+	salesPrice float32,
+) {
+	c := getCollyCollector()
 
-// 	c.OnHTML(".pp_mod-prod-desc-head", func(e *colly.HTMLElement) {
-// 		title = e.ChildText(".title")
-// 	})
+	// 상품명
+	c.OnHTML("h1.prod-title", func(h1 *colly.HTMLElement) {
+		productName = h1.Text
+	})
 
-// 	c.OnHTML(".product-gallery", func(e *colly.HTMLElement) {
-// 		e.ForEach(".js-item-image .item img", func(_ int, el *colly.HTMLElement) {
-// 			imageUrls = append(imageUrls, strings.Split(el.Attr("src"), "#")[0])
-// 		})
-// 	})
+	// 사이즈 & 재고
+	c.OnHTML(".siz-list-container", func(e *colly.HTMLElement) {
+		e.ForEach("li.emptyswatch", func(_ int, li *colly.HTMLElement) {
+			outOfStock := strings.Contains(li.Attr("class"), "notinstock")
+			size := li.ChildText("span.sizeDisplayValue")
+			stock := defaultStock
+			if outOfStock {
+				stock = 0
+			}
+			sizes = append(sizes, size)
+			inventories = append(inventories, domain.InventoryDAO{
+				Size:     size,
+				Quantity: stock,
+			})
+		})
+	})
 
-// 	c.OnHTML(".sizes .sizes-select-wrapper .sizes-select-list", func(e *colly.HTMLElement) {
-// 		e.ForEach(".list-inline li", func(_ int, el *colly.HTMLElement) {
-// 			size := el.ChildText("span .value")
-// 			sizes = append(sizes, size)
-// 			if el.Attr("class") != "li-disabled" {
-// 				inventories = append(inventories, domain.InventoryDAO{
-// 					Quantity: 10,
-// 					Size:     size,
-// 				})
-// 			}
-// 		})
-// 	})
+	// 설명
+	description = map[string]string{}
+	c.OnHTML(".titleDescPr.toggleMe", func(h2 *colly.HTMLElement) {
+		descriptionType := strings.ToLower(strings.TrimSpace(h2.Text))
+		descriptionKey := ""
+		switch descriptionType {
+		case "produktbeschreibung":
+			descriptionKey = "설명"
+		case "zusammensetzung":
+			descriptionKey = "소재"
+		}
+		if descriptionKey == "" {
+			// 다른 설명란의 정보는 쓰지 않음
+			return
+		}
 
-// 	c.OnHTML(".swatches", func(e *colly.HTMLElement) {
-// 		e.ForEach(".swatch", func(_ int, el *colly.HTMLElement) {
-// 			color := el.ChildAttr("img", "title")
-// 			colors = append(colors, color)
-// 		})
-// 	})
+		html, _ := h2.DOM.Next().Find(".detaildesc").Html()
+		nodes := strings.Split(html, "<br/>")
+		for _, node := range nodes {
+			text := strings.Replace(node, `<br \=""/>`, "", -1)
+			text = strings.Replace(text, `•`, "", -1)
+			text = strings.TrimSpace(text)
+			if strings.Contains(text, recommendingDescription) {
+				// "이런 상품과 함께 입으면 좋습니다"는 설명은 쓰지 않음
+				return
+			}
+			if strings.Contains(text, referenceDescription) {
+				// 레퍼런스 넘버는 쓰지 않음
+				return
+			}
+			if strings.Contains(text, inlineSizeDescription) {
+				// 설명 내부의 사이즈 정보는 쓰지 않음
+				return
+			}
+			key := descriptionKey
+			if strings.Contains(text, modelDescription) {
+				key = "모델"
+			}
 
-// 	c.OnHTML("#description .details-tab-content", func(e *colly.HTMLElement) {
-// 		description["설명"] = e.ChildText("p")
-// 	})
+			if val, exists := description[key]; exists {
+				description[key] = val + "\n" + text
+			} else {
+				description[key] = text
+			}
+		}
+	})
 
-// 	c.OnHTML("#composition .details-tab-content", func(e *colly.HTMLElement) {
-// 		texts := ""
-// 		e.ForEach("ul li", func(idx int, el *colly.HTMLElement) {
-// 			texts += el.Text
-// 		})
-// 		description["소재"] = texts
-// 	})
+	// 가격
+	c.OnHTML("span.price-sales", func(span *colly.HTMLElement) {
+		// 가격을 찾을 수 없으면 panic (MustCompile)
+		re := regexp.MustCompile("[0-9]+")
+		_salesPrice, _ := strconv.ParseFloat(re.FindAllString(span.Text, -1)[0], 32)
+		salesPrice = float32(_salesPrice)
+	})
+	c.OnHTML("span.price-standard", func(span *colly.HTMLElement) {
+		// 가격을 찾을 수 없으면 panic (MustCompile)
+		re := regexp.MustCompile("[0-9]+")
+		_originalPrice, _ := strconv.ParseFloat(re.FindAllString(span.Text, -1)[0], 32)
+		originalPrice = float32(_originalPrice)
+	})
 
-// 	c.OnHTML("#fitting .details-tab-content", func(e *colly.HTMLElement) {
-// 		texts := ""
-// 		e.ForEach("ul li", func(idx int, el *colly.HTMLElement) {
-// 			texts += el.Text
-// 		})
-// 		description["모델"] = texts
-// 	})
+	// 이미지
+	c.OnHTML("div.image-container", func(container *colly.HTMLElement) {
+		src, exists := container.DOM.Find("source").First().Attr("data-srcset")
+		if !exists {
+			return
+		}
+		imageUrl := strings.Split(src, "?")[0]
+		images = append(images, imageUrl)
+	})
 
-// 	c.Visit(productUrl)
-// 	return
-// }
+	c.Visit(productUrl)
+	return
+}
+
+func getCollyCollector() *colly.Collector {
+	c := colly.NewCollector(
+		colly.AllowedDomains(allowedDomain),
+		colly.UserAgent(userAgent),
+	)
+	return c
+}
