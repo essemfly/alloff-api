57b1324da4b02e9f9e9e5b61f7cf97dbeab3dd26
alloff-worker <essemfly@gmail.com>
2022-04-13 13:23:48 +0000

    Update workder deploy shell code

--- deploy_worker.sh
+++ deploy_worker.sh
@@ -4,3 +4,5 @@ sudo cp ./productsCrawler ./productsCrawler-last
 /usr/local/go/bin/go build -o productsCrawler ./cmd/productsCrawler/main.go
 sudo cp ./productDiffNotifier ./productDiffNotifier-last
 /usr/local/go/bin/go build -o productDiffNotifier ./cmd/productDiffNotifier/main.go
+sudo cp ./paymentSweeper ./paymentSweeper-last
+/usr/local/go/bin/go build -o paymentSweeper ./cmd/paymentSweeper/main.go
