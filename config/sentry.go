b39e4b2505457712727301402b13ab5ae51074bb
Seokmin Lee <essemfly@gmail.com>
2022-04-26 12:59:34 +0900

    Update changed sentry dns url

--- config/sentry.go
+++ config/sentry.go
@@ -9,7 +9,7 @@ import (
 func InitSentry(conf Configuration) {
 	if conf.ENVIRONMENT == "prod" {
 		err := sentry.Init(sentry.ClientOptions{
-			Dsn: "https://3bb7aa9c71b44397928e0101ebfecef2@o306501.ingest.sentry.io/6011225",
+			Dsn: "https://3bb7aa9c71b44397928e0101ebfecef2@o1042350.ingest.sentry.io/6011225",
 		})
 		if err != nil {
 			log.Fatalf("sentry.Init: %s", err)
