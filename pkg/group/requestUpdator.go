3071348acaf873f28b1b6825c50ffda73823acda
Sean Kim <58629967+ddhyun93@users.noreply.github.com>
2022-05-03 20:45:40 +0900

    Add func to check group request is possible to success or not

---
+++ pkg/group/requestUpdator.go
@@ -0,0 +1,44 @@
+package group
+
+import (
+	"github.com/lessbutter/alloff-api/config/ioc"
+	"github.com/lessbutter/alloff-api/internal/core/domain"
+	"log"
+)
+
+func CheckRequestPossible(request *domain.GroupRequestDAO) (bool, error) {
+	statusFilter := []domain.GroupRequestStatus{domain.GroupRequestStatusPending, domain.GroupRequestStatusSuccess}
+	groupRequests, err := ioc.Repo.GroupRequest.ListByGroupID(request.GroupID, statusFilter)
+	if err != nil {
+		log.Println("error on get group requests data : ", err)
+		return false, err
+	}
+	group, err := ioc.Repo.Groups.Get(request.GroupID)
+	if err != nil {
+		log.Println("error on get group data : ", err)
+		return false, err
+	}
+
+	if len(groupRequests) <= group.NumUserRequired {
+		return true, nil
+	} else {
+		rank := getRankOfRequest(request, groupRequests)
+		if rank <= group.NumUserRequired {
+			return true, nil
+		} else {
+			return false, nil
+		}
+	}
+}
+
+func getRankOfRequest(request *domain.GroupRequestDAO, groupRequests []*domain.GroupRequestDAO) (rank int) {
+	requestTime := request.CreatedAt
+	rank = len(groupRequests) + 1 // 꼴지 부터 시작
+	// 내 시간보다 느리게 요청한 시간을 만날때마다 순위를 1씩 올린다.
+	for _, gr := range groupRequests {
+		if gr.CreatedAt.After(requestTime) {
+			rank -= 1
+		}
+	}
+	return
+}
