3c331cb8bf87a7dd31420f5f2fa92d489df8d9f9
Sean Kim <58629967+ddhyun93@users.noreply.github.com>
2022-05-31 18:02:18 +0900

    Add theory official site crawler in theory module

--- pkg/crawler/malls/theory.go
+++ pkg/crawler/malls/theory.go
@@ -1,26 +1,43 @@
 package malls
 
 import (
-	"github.com/lessbutter/alloff-api/config"
-	"go.uber.org/zap"
-	"log"
-	"strconv"
-	"strings"
-
 	"github.com/gocolly/colly"
+	"github.com/lessbutter/alloff-api/config"
 	"github.com/lessbutter/alloff-api/config/ioc"
 	"github.com/lessbutter/alloff-api/internal/core/domain"
 	"github.com/lessbutter/alloff-api/pkg/crawler"
 	productinfo "github.com/lessbutter/alloff-api/pkg/productInfo"
+	"go.uber.org/zap"
+	"log"
+	"regexp"
+	"strconv"
+	"strings"
+	"time"
 )
 
 func CrawlTheory(worker chan bool, done chan bool, source *domain.CrawlSourceDAO) {
+	allowDomains := []string{"outlet.theory.com"}
+	productBaseUrl := "https://outlet.theory.com"
+	originalPriceSelector := ".price .product-price_compare .list .value"
+	discountedPriceSelector := ".price .product-price_sales .value"
+	productType := []domain.AlloffProductType{domain.Female}
+
+	if source.Category.Name == "남성세일" || source.Category.Name == "여성세일" {
+		if source.Category.Name == "남성세일" {
+			productType = []domain.AlloffProductType{domain.Male}
+		}
+		allowDomains = []string{"theory.com:443", "theory.com", "www.theory.com", "www.theory.com:443"}
+		productBaseUrl = "https://theory.com"
+		originalPriceSelector = ".attributes .price .strike-through .value"
+		discountedPriceSelector = ".attributes .price .sales .value"
+	}
+
 	c := colly.NewCollector(
-		colly.AllowedDomains("outlet.theory.com"),
+		colly.AllowedDomains(allowDomains...),
 		colly.UserAgent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11"),
 	)
+	c.SetRequestTimeout(60 * time.Second)
 
-	productBaseUrl := "https://outlet.theory.com"
 	totalProducts := 0
 
 	brand, err := ioc.Repo.Brands.GetByKeyname(source.Category.BrandKeyname)
@@ -29,23 +46,24 @@ func CrawlTheory(worker chan bool, done chan bool, source *domain.CrawlSourceDAO
 	}
 
 	c.OnHTML(".product-grid-tile--small", func(e *colly.HTMLElement) {
-		originalPriceStr := e.ChildText(".price .product-price_compare .list .value")
+		originalPriceStr := e.ChildText(originalPriceSelector)
 		originalPrice := 0.0
 		if originalPriceStr != "" {
 			originalPriceStr = strings.Replace(originalPriceStr, "Comparable Value:", "", -1)
 			originalPriceStr = strings.Replace(originalPriceStr, "Price reduced from", "", -1)
+			originalPriceStr = strings.Replace(originalPriceStr, "to", "", -1)
 			originalPriceStr = strings.Replace(originalPriceStr, "$", "", -1)
 			originalPriceStr = strings.Replace(originalPriceStr, ",", "", -1)
 			originalPriceStr = strings.Replace(originalPriceStr, "\n", "", -1)
 			originalPriceStr = strings.Trim(originalPriceStr, " ")
 			originalPrice, err = strconv.ParseFloat(originalPriceStr, 32)
 			if err != nil {
-				log.Println("err", err)
+				log.Printf("err on %s", err)
 				return
 			}
 		}
 
-		discountedPriceStr := e.ChildText(".price .product-price_sales .value")
+		discountedPriceStr := e.ChildText(discountedPriceSelector)
 		discountedPriceStr = strings.Replace(discountedPriceStr, "$", "", -1)
 		discountedPriceStr = strings.Replace(discountedPriceStr, ",", "", -1)
 		discountedPrice, err := strconv.ParseFloat(discountedPriceStr, 32)
@@ -65,13 +83,17 @@ func CrawlTheory(worker chan bool, done chan bool, source *domain.CrawlSourceDAO
 		productCode = productCode[len(productCode)-8:] // 앞에 카테고리 분류 지우면 순수 상품코드 추출
 
 		productName := e.ChildText(".link")
-		images, sizes, colors, inventories, description := getTheoryDetail(productUrl, productCode)
+		images, sizes, productColor, composition, inventories, description := getTheoryDetail(productUrl, productCode, allowDomains)
 
+		infos := map[string]string{
+			"소재": composition,
+			"색상": productColor,
+		}
 		addRequest := &productinfo.AddMetaInfoRequest{
 			AlloffName:          productName,
 			ProductID:           productId,
 			ProductUrl:          productUrl,
-			ProductType:         []domain.AlloffProductType{domain.Female},
+			ProductType:         productType,
 			OriginalPrice:       float32(originalPrice),
 			DiscountedPrice:     float32(discountedPrice),
 			CurrencyType:        domain.CurrencyUSD,
@@ -79,7 +101,8 @@ func CrawlTheory(worker chan bool, done chan bool, source *domain.CrawlSourceDAO
 			Source:              source,
 			AlloffCategory:      &domain.AlloffCategoryDAO{},
 			Images:              images,
-			Colors:              colors,
+			Colors:              []string{productColor},
+			Infos:               infos,
 			Sizes:               sizes,
 			Inventory:           inventories,
 			Information:         description,
@@ -92,7 +115,6 @@ func CrawlTheory(worker chan bool, done chan bool, source *domain.CrawlSourceDAO
 
 		totalProducts += 1
 		productinfo.ProcessCrawlingInfoRequests(addRequest)
-
 	})
 
 	err = c.Visit(source.CrawlUrl)
@@ -106,12 +128,16 @@ func CrawlTheory(worker chan bool, done chan bool, source *domain.CrawlSourceDAO
 	done <- true
 }
 
-func getTheoryDetail(productUrl, productCode string) (imageUrls, sizes, colors []string, inventories []*domain.InventoryDAO, description map[string]string) {
+func getTheoryDetail(productUrl, productCode string, allowDomains []string) (imageUrls, sizes []string, productColor, composition string, inventories []*domain.InventoryDAO, description map[string]string) {
 	c := colly.NewCollector(
-		colly.AllowedDomains("outlet.theory.com"),
+		colly.AllowedDomains(allowDomains...),
 		colly.UserAgent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11"),
 	)
+	c.SetRequestTimeout(60 * time.Second)
+
 	description = map[string]string{}
+
+	isDigit := regexp.MustCompile(`^\d*\.?\d+$`)
 
 	// images
 	c.OnHTML(".js-pdp-vertical-gallery", func(e *colly.HTMLElement) {
@@ -124,6 +150,9 @@ func getTheoryDetail(productUrl, productCode string) (imageUrls, sizes, colors [
 	c.OnHTML(".js-size-attributes", func(e *colly.HTMLElement) {
 		e.ForEach(".swatch-size ", func(_ int, el *colly.HTMLElement) {
 			size := el.ChildText("span")
+			if isDigit.MatchString(size) {
+				size = "US" + size
+			}
 			sizes = append(sizes, size)
 			unselectableSize := el.ChildText(".unselectable")
 			// 해당 사이즈가 선택 불가능한 사이즈가 아닐 경우 재고가 있는 걸로 판단
@@ -137,9 +166,9 @@ func getTheoryDetail(productUrl, productCode string) (imageUrls, sizes, colors [
 	})
 
 	// colors : 같은 디자인의 별개 색상 상품을 별개의 상품 id 로 구분하고 있음, 이때 상품 색상은 다 넣어줘야하는지 ?
-	c.OnHTML(".attributes", func(e *colly.HTMLElement) {
-		color := e.ChildText(".selected-attr")
-		colors = append(colors, color)
+	c.OnHTML(".attributes .attribute-title-color", func(e *colly.HTMLElement) {
+		originalColor := e.ChildText(".selected-color")
+		productColor = strings.TrimSpace(originalColor)
 	})
 
 	// descriptions
@@ -185,7 +214,6 @@ func getTheoryDetail(productUrl, productCode string) (imageUrls, sizes, colors [
 
 	// composition
 	c.OnHTML("div.pdp-composition", func(e *colly.HTMLElement) {
-		composition := ""
 		e.ForEach(".pdp-details-info", func(_ int, el *colly.HTMLElement) {
 			originalComposition := el.Text
 			originalComposition = strings.TrimSpace(originalComposition)
@@ -193,7 +221,6 @@ func getTheoryDetail(productUrl, productCode string) (imageUrls, sizes, colors [
 			composition += originalComposition + "\n"
 		})
 		composition = strings.TrimRight(composition, "\n")
-		description["소재"] = composition
 	})
 
 	c.OnHTML("div.pdp-care", func(e *colly.HTMLElement) {
