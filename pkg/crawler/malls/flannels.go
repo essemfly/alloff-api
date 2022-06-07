234a64f1d1c95e143bcf79a5335e056f54e93242
Seokmin Lee <essemfly@gmail.com>
2022-06-07 21:27:52 +0900

    WIP for getting details of flannels

--- pkg/crawler/malls/flannels.go
+++ pkg/crawler/malls/flannels.go
@@ -3,6 +3,7 @@ package malls
 import (
 	"encoding/json"
 	"log"
+	"net/url"
 	"strings"
 	"time"
 
@@ -10,6 +11,8 @@ import (
 	"github.com/lessbutter/alloff-api/config"
 	"github.com/lessbutter/alloff-api/internal/core/domain"
 	"github.com/lessbutter/alloff-api/internal/utils"
+	productinfo "github.com/lessbutter/alloff-api/pkg/productInfo"
+	"go.uber.org/zap"
 )
 
 // type ProductInfoJson struct {
@@ -100,8 +103,6 @@ func CrawlFlannels(worker chan bool, done chan bool, source *domain.CrawlSourceD
 	listQueryResp := &FlannelsListResponse{}
 	json.NewDecoder(resp.Body).Decode(listQueryResp)
 
-	log.Println("length of products", len(listQueryResp.Products))
-
 	typeVariations := []string{}
 	categoryVariations := []string{}
 	for _, filter := range listQueryResp.Filters {
@@ -122,108 +123,161 @@ func CrawlFlannels(worker chan bool, done chan bool, source *domain.CrawlSourceD
 	}
 
 	baseUrl := strings.Replace(source.CrawlUrl, "productsPerPage=10", "productsPerPage=1000", 1)
-	CrawlFlannelsProducts(baseUrl, typeVariations, categoryVariations)
+	productRequests := CrawlFlannelsProducts(baseUrl, typeVariations, categoryVariations)
 
-	// crawlURL, err := url.ParseQuery(source.CrawlUrl)
-	// log.Println("crwlUrl", crawlURL)
-	// if err != nil {
-	// 	config.Logger.Error("Parse error in source crawling "+source.ID.Hex(), zap.Error(err))
-	// }
-
-	// _, err = ioc.Repo.Brands.GetByKeyname(source.BrandKeyname)
-	// if err != nil {
-	// 	config.Logger.Error("error occurred on get brand by key name : ", zap.Error(err))
-	// }
-
+	log.Println("length of requests for brand", source.BrandKeyname, len(productRequests))
 	<-worker
 	done <- true
 }
 
-func CrawlFlannelsProducts(baseUrl string, productTypes []string, categories []string) {
-	urls := []string{}
-	if len(productTypes) == 0 {
-		for _, cat := range categories {
-			urlparams = append(urlparams, "&selectedFilters=1147_726918^"+cat)
-		}
-		return
-	}
-
+func CrawlFlannelsProducts(baseUrl string, productTypes []string, categories []string) []*productinfo.AddMetaInfoRequest {
+	productRequests := []*productinfo.AddMetaInfoRequest{}
 	for _, cat := range categories {
 		for _, ptype := range productTypes {
-			urlparams = append(urlparams, "&selectedFilters=AFLOR^"+ptype+"|1147_726918^"+cat)
+			paramQuery := "AFLOR^" + ptype + "|1147_726918^" + cat
+			catUrl := baseUrl + "&selectedFilters=" + url.QueryEscape(paramQuery)
+			productRequests = append(productRequests, GetFlannelProductList(catUrl, ptype, cat)...)
 		}
 	}
-
-	return
+	return productRequests
 }
 
-func getFlannelsDetail(productUrl string) (images, colors []string, infos, description map[string]string, notSale bool) {
+func GetFlannelProductList(categoryUrl, productType, categoryName string) []*productinfo.AddMetaInfoRequest {
+	productTypes := map[string][]domain.AlloffProductType{
+		"Mens":          {domain.Male},
+		"Womens":        {domain.Female},
+		"Boys":          {domain.Boy},
+		"Girls":         {domain.Girl},
+		"Unisex Kids":   {domain.Boy, domain.Girl},
+		"Unisex Adults": {domain.Male, domain.Female},
+	}
+
+	errorMessage := "Crawl Failed: ProductURL: " + categoryUrl
+	resp, err := utils.RequestRetryer(categoryUrl, utils.REQUEST_GET, utils.GetFlannelsHeader(), "", errorMessage)
+	if err != nil {
+		log.Println("flannels fail on: ", categoryUrl)
+		config.Logger.Error("product list crawling error "+categoryUrl, zap.Error(err))
+		return nil
+	}
+	defer resp.Body.Close()
+
+	listQueryResp := &FlannelsListResponse{}
+	json.NewDecoder(resp.Body).Decode(listQueryResp)
+
+	log.Println("length of products: ", categoryName+" "+productType, len(listQueryResp.Products))
+
+	baseURL := "https://www.flannels.com"
+	requests := []*productinfo.AddMetaInfoRequest{}
+	for _, pd := range listQueryResp.Products {
+		newRequest := GetFlannelsDetail(baseURL + pd.URL)
+		newRequest.ProductType = productTypes[productType]
+		requests = append(requests, newRequest)
+	}
+
+	return requests
+}
+
+func GetFlannelsDetail(productURL string) *productinfo.AddMetaInfoRequest {
 	c := colly.NewCollector(
-		colly.AllowedDomains("www.afound.com"),
+		colly.AllowedDomains("www.flannels.com"),
 		colly.UserAgent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11"),
 	)
 	c.SetRequestTimeout(10 * time.Second)
 
-	//isDigit := regexp.MustCompile(`^\d*\.?\d+$`)
-
-	infos = map[string]string{
-		"소재": "",
-		"색상": "",
+	productRequest := &productinfo.AddMetaInfoRequest{
+		// AlloffName:          pd.ProductName,
+		// ProductID:           pd.ProductCode,
+		ProductUrl: productURL,
+		// OriginalPrice:       float32(originalPriceInt),
+		// DiscountedPrice:     float32(discountedPriceInt),
+		// CurrencyType:        domain.CurrencyKRW,
+		// Brand:               brand,
+		// Source:              source,
+		// AlloffCategory:      &domain.AlloffCategoryDAO{},
+		// Images:              images,
+		// Colors:              colors,
+		// Sizes:               sizes,
+		// Inventory:           inventories,
+		// Information:         description,
+		// IsForeignDelivery:   false,
+		// IsTranslateRequired: false,
+		// ModuleName:          source.CrawlModuleName,
+		// IsRemoved:           false,
+		// IsSoldout:           false,
 	}
-	description = map[string]string{
-		"사이즈 및 핏": "",
-		"제품설명":    "",
-	}
-	images = []string{}
-	colors = []string{}
-	sizeAndFit := ""
 
-	// 상품 url에 접속했지만 상품 정보가 없는경우 리스트로 리다이렉트된다. 이떄 리스트로 리다이렉트 됐는지를 판단하여 상품 유효성을 검증한다.
-	c.OnHTML(".af-product-listing__category-filters__section__header--wrapper", func(e *colly.HTMLElement) {
-		if e.Text != "" {
-			notSale = true
-			return
-		}
-	})
+	c.Visit(productURL)
 
-	c.OnHTML("div.product-page__grid > section.product-page__grid-col-right > div.af-accordion.pdp-accordion.spaced--xmedium > "+
-		"section:nth-child(1) > div > p", func(e *colly.HTMLElement) {
-		desc := e.Text
-		desc = strings.TrimSpace(desc)
-		description["제품설명"] = desc
-	})
-
-	c.OnHTML("div.product-page__grid > section.product-page__grid-col-right > div.af-accordion.pdp-accordion.spaced--xmedium > "+
-		"section:nth-child(1) > div > div > dl:nth-child(6) > dd", func(e *colly.HTMLElement) {
-		sizeAndFit += e.Text + ", "
-	})
-	c.OnHTML("div.product-page__grid > section.product-page__grid-col-right > div.af-accordion.pdp-accordion.spaced--xmedium > "+
-		"section:nth-child(1) > div > div > dl:nth-child(5) > dd", func(e *colly.HTMLElement) {
-		sizeAndFit += e.Text
-	})
-
-	c.OnHTML("div.product-page__grid > section.product-page__grid-col-right > div.af-accordion.pdp-accordion.spaced--xmedium > "+
-		"section:nth-child(1) > div > div > dl:nth-child(1) > dd", func(e *colly.HTMLElement) {
-		infos["소재"] = e.Text
-	})
-
-	c.OnHTML("div.product-page__grid > section.product-page__grid-col-right > div.af-accordion.pdp-accordion.spaced--xmedium > "+
-		"section:nth-child(1) > div > div > dl:nth-child(2) > dd", func(e *colly.HTMLElement) {
-		infos["색상"] = e.Text
-		colors = append(colors, e.Text)
-	})
-
-	c.OnHTML("div.product-page__grid > section.product-page__grid-col-left", func(e *colly.HTMLElement) {
-		e.ForEach("a", func(_ int, el *colly.HTMLElement) {
-			imgUrl := el.ChildAttr("img", "src")
-			imgUrl = strings.Split(imgUrl, "?")[0]
-			imgUrl += "?preset=product-details-desktop"
-			images = append(images, imgUrl)
-		})
-	})
-
-	c.Visit(productUrl)
-	description["사이즈 및 핏"] = sizeAndFit
-
-	return
+	return productRequest
 }
+
+// func getFlannelsDetail(productUrl string) (images, colors []string, infos, description map[string]string, notSale bool) {
+// 	c := colly.NewCollector(
+// 		colly.AllowedDomains("www.afound.com"),
+// 		colly.UserAgent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11"),
+// 	)
+// 	c.SetRequestTimeout(10 * time.Second)
+
+// 	//isDigit := regexp.MustCompile(`^\d*\.?\d+$`)
+
+// 	infos = map[string]string{
+// 		"소재": "",
+// 		"색상": "",
+// 	}
+// 	description = map[string]string{
+// 		"사이즈 및 핏": "",
+// 		"제품설명":    "",
+// 	}
+// 	images = []string{}
+// 	colors = []string{}
+// 	sizeAndFit := ""
+
+// 	// 상품 url에 접속했지만 상품 정보가 없는경우 리스트로 리다이렉트된다. 이떄 리스트로 리다이렉트 됐는지를 판단하여 상품 유효성을 검증한다.
+// 	c.OnHTML(".af-product-listing__category-filters__section__header--wrapper", func(e *colly.HTMLElement) {
+// 		if e.Text != "" {
+// 			notSale = true
+// 			return
+// 		}
+// 	})
+
+// 	c.OnHTML("div.product-page__grid > section.product-page__grid-col-right > div.af-accordion.pdp-accordion.spaced--xmedium > "+
+// 		"section:nth-child(1) > div > p", func(e *colly.HTMLElement) {
+// 		desc := e.Text
+// 		desc = strings.TrimSpace(desc)
+// 		description["제품설명"] = desc
+// 	})
+
+// 	c.OnHTML("div.product-page__grid > section.product-page__grid-col-right > div.af-accordion.pdp-accordion.spaced--xmedium > "+
+// 		"section:nth-child(1) > div > div > dl:nth-child(6) > dd", func(e *colly.HTMLElement) {
+// 		sizeAndFit += e.Text + ", "
+// 	})
+// 	c.OnHTML("div.product-page__grid > section.product-page__grid-col-right > div.af-accordion.pdp-accordion.spaced--xmedium > "+
+// 		"section:nth-child(1) > div > div > dl:nth-child(5) > dd", func(e *colly.HTMLElement) {
+// 		sizeAndFit += e.Text
+// 	})
+
+// 	c.OnHTML("div.product-page__grid > section.product-page__grid-col-right > div.af-accordion.pdp-accordion.spaced--xmedium > "+
+// 		"section:nth-child(1) > div > div > dl:nth-child(1) > dd", func(e *colly.HTMLElement) {
+// 		infos["소재"] = e.Text
+// 	})
+
+// 	c.OnHTML("div.product-page__grid > section.product-page__grid-col-right > div.af-accordion.pdp-accordion.spaced--xmedium > "+
+// 		"section:nth-child(1) > div > div > dl:nth-child(2) > dd", func(e *colly.HTMLElement) {
+// 		infos["색상"] = e.Text
+// 		colors = append(colors, e.Text)
+// 	})
+
+// 	c.OnHTML("div.product-page__grid > section.product-page__grid-col-left", func(e *colly.HTMLElement) {
+// 		e.ForEach("a", func(_ int, el *colly.HTMLElement) {
+// 			imgUrl := el.ChildAttr("img", "src")
+// 			imgUrl = strings.Split(imgUrl, "?")[0]
+// 			imgUrl += "?preset=product-details-desktop"
+// 			images = append(images, imgUrl)
+// 		})
+// 	})
+
+// 	c.Visit(productUrl)
+// 	description["사이즈 및 핏"] = sizeAndFit
+
+// 	return
+// }
