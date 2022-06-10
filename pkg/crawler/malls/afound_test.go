71c92a633c13376d6f9b82ac3becf1fe0aae2e94
Sean Kim <58629967+ddhyun93@users.noreply.github.com>
2022-06-10 18:47:18 +0900

    Deactivate test codes

--- pkg/crawler/malls/afound_test.go
+++ pkg/crawler/malls/afound_test.go
@@ -1,18 +1,18 @@
 package malls
 
-import (
-	"github.com/stretchr/testify/require"
-	"testing"
-)
-
-func TestAfoundCrawler(t *testing.T) {
-	t.Run("test getAfoundDetail", func(t *testing.T) {
-
-		productUrl := "https://www.afound.com/de-de/produkte/cropped-teddy-bomber-jacket-dark-khaki-green_9fae92hb"
-		images, colors, _, _, notSale := getAfoundDetail(productUrl)
-		if !notSale {
-			require.Equal(t, []string{"Grün"}, colors)
-			require.NotEqual(t, 0, len(images))
-		}
-	})
-}
+//import (
+//	"github.com/stretchr/testify/require"
+//	"testing"
+//)
+//
+//func TestAfoundCrawler(t *testing.T) {
+//	t.Run("test getAfoundDetail", func(t *testing.T) {
+//
+//		productUrl := "https://www.afound.com/de-de/produkte/cropped-teddy-bomber-jacket-dark-khaki-green_9fae92hb"
+//		images, colors, _, _, notSale := getAfoundDetail(productUrl)
+//		if !notSale {
+//			require.Equal(t, []string{"Grün"}, colors)
+//			require.NotEqual(t, 0, len(images))
+//		}
+//	})
+//}
