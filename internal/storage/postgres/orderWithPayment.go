5aae4aefe926c22f50a563cfe143fd448bf21193
Seokmin Lee <essemfly@gmail.com>
2022-06-27 16:23:19 +0900

    Remove cancel payment on request payment

--- internal/storage/postgres/orderWithPayment.go
+++ internal/storage/postgres/orderWithPayment.go
@@ -181,7 +181,6 @@ func (repo *orderPaymentService) RequestPayment(orderDao *domain.OrderDAO, payme
 		if orderDao.OrderStatus != domain.ORDER_PAYMENT_PENDING {
 			return fmt.Errorf("ERR400:already ongoing order finished")
 		}
-		repo.CancelPayment(orderDao, paymentDao)
 	}
 
 	// 이제 Stock 옵션 줄이면 된다. + Order의 상태 및 timestamp찍으면 된다.
