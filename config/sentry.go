d4d81a3cb2ec146b9dce811219cf366a4575c3ee
Seokmin Lee <essemfly@gmail.com>
2022-06-28 18:07:44 +0900

    Update sentry dsn

--- config/sentry.go
+++ config/sentry.go
@@ -10,7 +10,7 @@ import (
 func InitSentry() {
 	if viper.GetString("ENVIRONMENT") == "prod" {
 		err := sentry.Init(sentry.ClientOptions{
-			Dsn: "https://3bb7aa9c71b44397928e0101ebfecef2@o1042350.ingest.sentry.io/6011225",
+			Dsn: "https://3bb7aa9c71b44397928e0101ebfecef2:dd53f2cf3eec49ea8e1ce7967f1fe3ce@o1042350.ingest.sentry.io/6011225",
 		})
 		if err != nil {
 			log.Fatalf("sentry.Init: %s", err)
